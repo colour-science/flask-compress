22237d3d6fab2b1b924c6a1a56445d5a18a748f9
Anthony Nemitz <anemitz@gmail.com>
2012-10-10 13:11:17 -0700

    cleanup

--- flask_gzip.py
+++ flask_gzip.py
@@ -10,10 +10,13 @@ class Gzip(object):
         self.app.after_request(self.after_request)
 
     def after_request(self, response):
-        if request.headers.get('Accept-Encoding'):
-            encodings = request.headers.get('Accept-Encoding').split(',')
-            if 'gzip' not in encodings:
-                return response
+        accept_encoding = request.headers.get('Accept-Encoding', '') 
+        if not accept_encoding:
+            return response
+
+        encodings = accept_encoding.split(',')
+        if 'gzip' not in encodings:
+            return response
             
         if (200 > response.status_code >= 300) or len(response.data) < 500 or 'Content-Encoding' in response.headers:
             return response
@@ -25,4 +28,5 @@ class Gzip(object):
         response.data = gzip_buffer.getvalue()
         response.headers['Content-Encoding'] = 'gzip'
         response.headers['Content-Length'] = len(response.data)
+
         return response

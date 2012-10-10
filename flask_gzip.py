95a8f95407f403dfc65e170e11d893e7e052dab5
Anthony Nemitz <anemitz@gmail.com>
2012-10-10 12:47:12 -0700

    initial

---
+++ flask_gzip.py
@@ -0,0 +1,28 @@
+import gzip
+import StringIO
+from flask import request
+
+
+class Gzip(object):
+    def __init__(self, app, compress_level=6):
+        self.app = app
+        self.compress_level = compress_level
+        self.app.after_request(self.after_request)
+
+    def after_request(self, response):
+        if request.headers.get('Accept-Encoding'):
+            encodings = request.headers.get('Accept-Encoding').split(',')
+            if 'gzip' not in encodings:
+                return response
+            
+        if (200 > response.status_code >= 300) or len(response.data) < 500 or 'Content-Encoding' in response.headers:
+            return response
+        
+        gzip_buffer = StringIO.StringIO()
+        gzip_file = gzip.GzipFile(mode='wb', compresslevel=self.compress_level, fileobj=gzip_buffer)
+        gzip_file.write(response.data)
+        gzip_file.close()
+        response.data = gzip_buffer.getvalue()
+        response.headers['Content-Encoding'] = 'gzip'
+        response.headers['Content-Length'] = len(response.data)
+        return response

dafb316b957e117ca7fd4c282d2828c489475018
William Fagan <will@wichitacode.com>
2015-10-08 11:55:49 -0700

    Bump version for release.

--- setup.py
+++ setup.py
@@ -9,7 +9,7 @@ from setuptools import setup
 
 setup(
     name='Flask-Compress',
-    version='1.2.1',
+    version='1.3.0',
     url='https://github.com/wichitacode/flask-compress',
     license='MIT',
     author='William Fagan',

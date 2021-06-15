colour-science/flask-compress

commit af2f16335279044e8baeab42e0b182d3e05861b4

Create an actual package, for easier distribution

setup.py
@@ -1,9 +1,9 @@
-import setuptools
+from setuptools import setup, find_packages
 
 with open('README.md') as fl:
     LONG_DESCRIPTION = fl.read()
 
-setuptools.setup(
+setup(
     name='Flask-Compress',
     use_scm_version=True,
     url='https://github.com/colour-science/flask-compress',
@@ -13,7 +13,7 @@ setuptools.setup(
     description='Compress responses in your Flask app with gzip, deflate or brotli.',
     long_description=LONG_DESCRIPTION,
     long_description_content_type='text/markdown',
-    py_modules=['flask_compress'],
+    packages=find_packages(),
     zip_safe=False,
     include_package_data=True,
     platforms='any',

flask_compress/__init__.py
@@ -1,0 +1,1 @@
+from .flask_compress import Compress

commit c82a993548494ee13035ca77aeaf0726218bee0d

Do not install top-level "tests" package
Fixes a regression in af2f16335279044e8baeab42e0b182d3e05861b4 that
causes "tests" to be installed as a top-level package to site-packages.

setup.py
@@ -13,7 +13,7 @@ setup(
     description='Compress responses in your Flask app with gzip, deflate or brotli.',
     long_description=LONG_DESCRIPTION,
     long_description_content_type='text/markdown',
-    packages=find_packages(),
+    packages=find_packages(exclude=['tests']),
     zip_safe=False,
     include_package_data=True,
     platforms='any',
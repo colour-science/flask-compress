52b23960ed31b6fecdfd70b2da641c0fbd1e98ff
Alex <alexprengere@gmail.com>
2025-08-04 11:07:08 +0200

    Use backports.zstd instead of pyzstd

--- flask_compress/compat.py
+++ flask_compress/compat.py
@@ -7,20 +7,20 @@
 # When Python 3.14 is the lowest supported version,
 # the `import compression.*` statements can move to `flask_compress.py`
 # and this try/except block can be removed from this file.
-try:
-    # Python >= 3.14
+import sys
+
+if sys.version_info >= (3, 14):
     import compression.gzip
     import compression.zlib
     import compression.zstd
-except ModuleNotFoundError:
-    # Python <= 3.13
+else:  # Python <= 3.13
     import gzip
     import types
     import zlib
 
-    import pyzstd
+    from backports import zstd
 
     compression = types.SimpleNamespace()
     compression.gzip = gzip
     compression.zlib = zlib
-    compression.zstd = pyzstd
+    compression.zstd = zstd
